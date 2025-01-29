1d3cffec7256883104e041160e7d643891a6d047
HarukaB <100848181+HarukaB@users.noreply.github.com>
2025-01-29 00:01:18 -0800

    Modify isToolSuitable method

--- src/main/java/top/diaoyugan/vein_mine/utils/Utils.java
+++ src/main/java/top/diaoyugan/vein_mine/utils/Utils.java
@@ -1,11 +1,15 @@
 package top.diaoyugan.vein_mine.utils;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 
 public class Utils {
     public static boolean isToolSuitable(BlockState blockState, PlayerEntity player){
         ItemStack tool = player.getMainHandStack();
-        return blockState.isToolRequired() && tool.isSuitableFor(blockState);
+        if (blockState.isToolRequired()){
+            return tool.isSuitableFor(blockState);
+        }
+        return true;
     }
 }
