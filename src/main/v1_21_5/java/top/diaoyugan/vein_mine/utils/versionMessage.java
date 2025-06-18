4fabc9d3330e59b33d235bbc751f2a99ae9ac8d1
diaoyugan <67471429+diaoyugan@users.noreply.github.com>
2025-06-18 10:32:41 +0800

    how ?

--- src/main/v1_21_5/java/top/diaoyugan/vein_mine/utils/versionMessage.java
+++ src/main/v1_21_5/java/top/diaoyugan/vein_mine/utils/versionMessage.java
@@ -5,11 +5,12 @@ import net.minecraft.text.Text;
 
 import java.util.Objects;
 
-public class versionMessage implements utilsVersionInterface{
+public class versionMessage implements utilsVersionInterface {
     @Override
     public void sendMessage(ServerPlayerEntity player, Text message, Boolean isOnActionbar) {
         if (isOnActionbar)
             player.server.execute(() -> player.sendMessage(message, true));
         else
             player.server.execute(() -> player.sendMessage(message, false));
+    }
 }
