cfc3a1d24f8c7332638d354edb59bb07232613f4
HarukaB <100848181+HarukaB@users.noreply.github.com>
2025-07-05 10:15:51 +0800

    feat: support 1.21.3

---
+++ src/main/v1_21_3/java/top/diaoyugan/vein_mine/utils/VersionMessage.java
@@ -0,0 +1,16 @@
+package top.diaoyugan.vein_mine.utils;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+import java.util.Objects;
+
+public class VersionMessage implements IUtilsVersion {
+    @Override
+    public void sendMessage(ServerPlayerEntity player, Text message, Boolean isOnActionbar) {
+        if (isOnActionbar)
+            player.server.execute(() -> player.sendMessage(message, true));
+        else
+            player.server.execute(() -> player.sendMessage(message, false));
+    }
+}
