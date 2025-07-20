e4a76da35dd78b3f03781558d808182ee9f4a7a0
HarukaB <100848181+HarukaB@users.noreply.github.com>
2025-07-20 18:31:34 +0800

    feat: add 1.21.8 support (Exp)

---
+++ src/main/v1_21_8/java/top/diaoyugan/vein_mine/mixin/VeinMineMixinPlugin.java
@@ -0,0 +1,50 @@
+package top.diaoyugan.vein_mine.mixin;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import top.diaoyugan.vein_mine.config.IntrusiveConfig;
+import top.diaoyugan.vein_mine.utils.Utils;
+
+import java.util.List;
+import java.util.Set;
+
+public class VeinMineMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.endsWith("RenderLayerAccessor")) {
+            return IntrusiveConfig.isEnabled(); // 控制是否注入
+        }
+        return true;
+    }
+
+    // 其余接口方法保留默认返回（空实现即可）
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+}
---
+++ src/main/v1_21_8/java/top/diaoyugan/vein_mine/utils/VersionMessage.java
@@ -0,0 +1,16 @@
+package top.diaoyugan.vein_mine.utils;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+import java.util.Objects;
+
+public class VersionMessage implements IUtilsVersion {
+    @Override
+    public void sendMessage(ServerPlayerEntity player, Text message, Boolean isOnActionbar) {
+        if (isOnActionbar)
+            Objects.requireNonNull(player.getServer()).execute(() -> player.sendMessage(message, true));
+        else
+            Objects.requireNonNull(player.getServer()).execute(() -> player.sendMessage(message, false));
+    }
+}
