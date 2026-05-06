595673c05315f3acde8020ba4850a5482e7cc13a
diaoyugan <67471429+diaoyugan@users.noreply.github.com>
2026-05-06 08:52:04 +0800

    update dependencies

--- neoforge/src/main/java/top/diaoyugan/veinmine/events/PlayerBreakBlock.java
+++ neoforge/src/main/java/top/diaoyugan/veinmine/events/PlayerBreakBlock.java
@@ -8,12 +8,13 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 import top.diaoyugan.veinmine.Constants;
 
 @EventBusSubscriber(modid = Constants.ID)
 public class PlayerBreakBlock {
     @SubscribeEvent
-    public static void onBlockBreak(BlockEvent.BreakEvent event) {
+    public static void onBlockBreak(BreakBlockEvent event) {
         Player player = event.getPlayer();
         BlockPos pos = event.getPos();
         BlockState state = event.getState();
